3632eba6f8a09e4b7205909150ee76501d853650
Kathryn Oden <odenkate1203@gmail.com>
2026-05-10 20:09:41 -0500

    made randomized test boxes show up on screen

--- src/project.py
+++ src/project.py
@@ -10,9 +10,26 @@ screen_height = 450
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Crochet Pattern Maker')
 
+#test boxes, replace with crochet stitches later
+active_box = None
+images = []
+for i in range(5):
+    x = random.randint(50, 700)
+    y = random.randint(50, 350)
+    w = random.randint(35, 65)
+    h = random.randint(35, 65)
+    box = pygame.Rect(x, y, w, h)
+    images.append(box)
+
 run = True
 while run:
     screen.fill((0, 255, 0))
+    purple = (255, 0, 0)
+
+    #update and draw items
+    for box in images:
+        pygame.draw.rect(screen, purple, box)
+
     pygame.display.flip()
 
     for event in pygame.event.get():
