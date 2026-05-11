3a501b99d8fda74c7c3a1e2b5f4f30daf41ae9d2
Kathryn Oden <odenkate1203@gmail.com>
2026-05-11 10:01:15 -0500

    can only click the buttons once, so no adding 30 chain stitches in one click :D

--- src/project.py
+++ src/project.py
@@ -27,6 +27,7 @@ class Button():
         self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
+        self.clicked = False
 
     def draw(self):
 
@@ -35,8 +36,12 @@ class Button():
         
         #check mouseover and clicked conditions
         if self.rect.collidepoint(pos):
-            if pygame.mouse.get_pressed()[0]:
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
                 print("CLICKED")
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
 
         #draw button on screen
         screen.blit(self.image, (self.rect.x, self.rect.y))
