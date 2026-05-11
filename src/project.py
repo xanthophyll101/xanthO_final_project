9bef6d1776bed7ea610d9517d64228c04a31cb63
Kathryn Oden <odenkate1203@gmail.com>
2026-05-11 10:14:24 -0500

    organized the code (added main() function, forgot to do that in the beginning oops)

--- src/project.py
+++ src/project.py
@@ -1,25 +1,6 @@
 import pygame
     
 
-pygame.init()
-
-#game window
-screen_width = 800
-screen_height = 600
-
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Crochet Pattern Maker')
-clock = pygame.time.Clock()
-
-#load button images
-chain_button_img = pygame.image.load('chain_button.jpg')
-singleCR_button_img = pygame.image.load('singleCR_button.jpg')
-doubleCR_button_img = pygame.image.load('doubleCR_button.jpg')
-
-dt = 0
-mouse_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
-#Button class
 class Button():
     def __init__(self, x, y, image, scale):
         width = image.get_width()
@@ -29,7 +10,9 @@ class Button():
         self.rect.topleft = (x, y)
         self.clicked = False
 
-    def draw(self):
+    def draw(self, screen):
+
+        action = False
 
         #get mouse position
         pos = pygame.mouse.get_pos()
@@ -38,46 +21,72 @@ class Button():
         if self.rect.collidepoint(pos):
             if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
                 self.clicked = True
-                print("CLICKED")
+                action = True
 
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
+            action = False
 
         #draw button on screen
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
-
-#create button instances
-chain_button = Button(100, 200, chain_button_img, 2)
-singleCR_button = Button(250, 200, singleCR_button_img, 2)
-doubleCR_button = Button(400, 200, doubleCR_button_img, 2)
+        return action
 
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+def main():
+    #initialize screen & clock
+    pygame.init()
+    screen_width = 800
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption('Crochet Pattern Maker')
+    clock = pygame.time.Clock()
 
-    screen.fill("#8fa9cc")
+    #buttons
+    chain_button_img = pygame.image.load('chain_button.jpg')
+    singleCR_button_img = pygame.image.load('singleCR_button.jpg')
+    doubleCR_button_img = pygame.image.load('doubleCR_button.jpg')
+    #create button instances
+    chain_button = Button(100, 200, chain_button_img, 2)
+    singleCR_button = Button(250, 200, singleCR_button_img, 2)
+    doubleCR_button = Button(400, 200, doubleCR_button_img, 2)
 
-    chain_button.draw()
-    singleCR_button.draw()
-    doubleCR_button.draw()
+    dt = 0
+    mouse_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-    #circle & moving the circle
-    #pygame.draw.circle(screen, "#b33e3e", mouse_pos, 40)
+    #event loop - put everything that renders onscreen here
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-    if pygame.mouse.get_pressed()[0]:
-        if event.type == pygame.MOUSEMOTION:
-            pos = pygame.mouse.get_pos()
-            mouse_pos.x = pos[0]
-            mouse_pos.y = pos[1]
+        screen.fill("#8fa9cc")
+
+        if chain_button.draw(screen):  #basically if action == True
+            print("Action works! Yay!")
+        if singleCR_button.draw(screen):
+            print('singleCR works!')
+        if doubleCR_button.draw(screen):
+            print('doubleCr works!')
+
+        #circle & moving the circle
+        #pygame.draw.circle(screen, "#b33e3e", mouse_pos, 40)
+
+        if pygame.mouse.get_pressed()[0]:
+            if event.type == pygame.MOUSEMOTION:
+                pos = pygame.mouse.get_pos()
+                mouse_pos.x = pos[0]
+                mouse_pos.y = pos[1]
 
 
-    pygame.display.flip()
+        pygame.display.flip()
 
 
-    dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
-pygame.quit()
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
